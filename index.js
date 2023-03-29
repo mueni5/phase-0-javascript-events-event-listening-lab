5441ce4ec6acbc61864cd8a89ea6388916a88df9
+ <+>
2023-03-29 10:43:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+function addingEventListener(){
+const input = document.getElementById("input");
+
+input.addEventListener("click", clickAlert);
+
+function clickAlert(){
+    alert("I was clicked!");
+  };
 }
